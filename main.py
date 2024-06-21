ea006769a6a62b5f4c29c2f68f70337c3cab156b
Jean Roelens <jean.roelens@ynov.com>
2024-06-21 15:15:10 +0200

    added substraction

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ class SimpleMath:
     @staticmethod
     def addition(a, b):
         return a + b
+    
+    @staticmethod
+    def subtraction(a, b):
+        return a - b
 
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
@@ -15,6 +19,17 @@ class TestSimpleMath(unittest.TestCase):
         
         result = SimpleMath.addition(0, 0)
         self.assertEqual(result, 0)
+    
+    def test_subtraction(self):
+        result = SimpleMath.subtraction(5, 3)
+        self.assertEqual(result, 2)
+        
+        result = SimpleMath.subtraction(-1, 1)
+        self.assertEqual(result, -2)
+        
+        result = SimpleMath.subtraction(0, 0)
+        self.assertEqual(result, 0)
 
 if __name__ == '__main__':
     unittest.main()
+
